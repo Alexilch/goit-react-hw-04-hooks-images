392455a82198d9089345edcb7e80ea1407ba736d
Alexander Ilchenko <ilchenko.alexander.n@gmail.com>
2021-12-16 10:33:35 +0200

    add modal hooks

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import { createPortal } from 'react-dom';
-import React, { Component } from 'react';
+import { useEffect, useCallback } from 'react';
+// import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 import '../icons/arrow_back_ios_black_24dp.svg';
@@ -7,63 +8,123 @@ import '../icons/arrow_forward_ios_black_24dp.svg';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handlekeyDown);
-  }
+export default function Modal({
+  imageModal,
+  onClose,
+  onArrowLeft,
+  onArrowRight,
+}) {
+  const handlekeyDown = useCallback(
+    event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+      if (event.key === 'ArrowLeft') {
+        onArrowLeft();
+      }
+      if (event.key === 'ArrowRight') {
+        onArrowRight();
+      }
+    },
+    [onClose, onArrowLeft, onArrowRight],
+  );
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handlekeyDown);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handlekeyDown);
+    return () => window.removeEventListener('keydown', handlekeyDown);
+  }, [handlekeyDown]);
 
-  handlekeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-    if (event.key === 'ArrowLeft') {
-      this.props.onArrowLeft();
-    }
-    if (event.key === 'ArrowRight') {
-      this.props.onArrowRight();
-    }
+  const handleLeft = () => {
+    onArrowLeft();
   };
 
-  handleLeft = () => {
-    this.props.onArrowLeft();
+  const handleRight = () => {
+    onArrowRight();
   };
 
-  handleRight = () => {
-    this.props.onArrowRight();
-  };
-
-  handleBackDropClick = event => {
+  const handleBackDropClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { imageModal } = this.props;
-    return createPortal(
-      <div className={s.overlay} onClick={this.handleBackDropClick}>
-        <div className={s.modal}>
-          <button
-            className={s.leftarrow}
-            type="button"
-            onClick={() => this.handleLeft()}
-          ></button>
-          <img src={imageModal} alt="" />
-          <button
-            className={s.rightarrow}
-            type="button"
-            onClick={() => this.handleRight()}
-          ></button>
-        </div>
-      </div>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <div className={s.overlay} onClick={handleBackDropClick}>
+      <div className={s.modal}>
+        <button
+          className={s.leftarrow}
+          type="button"
+          onClick={() => handleLeft()}
+        ></button>
+        <img src={imageModal} alt="" />
+        <button
+          className={s.rightarrow}
+          type="button"
+          onClick={() => handleRight()}
+        ></button>
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
+
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handlekeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handlekeyDown);
+//   }
+
+//   handlekeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//     if (event.key === 'ArrowLeft') {
+//       this.props.onArrowLeft();
+//     }
+//     if (event.key === 'ArrowRight') {
+//       this.props.onArrowRight();
+//     }
+//   };
+
+//   handleLeft = () => {
+//     this.props.onArrowLeft();
+//   };
+
+//   handleRight = () => {
+//     this.props.onArrowRight();
+//   };
+
+//   handleBackDropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { imageModal } = this.props;
+//     return createPortal(
+//       <div className={s.overlay} onClick={this.handleBackDropClick}>
+//         <div className={s.modal}>
+//           <button
+//             className={s.leftarrow}
+//             type="button"
+//             onClick={() => this.handleLeft()}
+//           ></button>
+//           <img src={imageModal} alt="" />
+//           <button
+//             className={s.rightarrow}
+//             type="button"
+//             onClick={() => this.handleRight()}
+//           ></button>
+//         </div>
+//       </div>,
+//       modalRoot,
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   modalImg: PropTypes.string,
